c8e41c3f9dbe095033b689b79a258add3b2f3497
Minibox <minibox724@gmail.com>
2023-07-04 07:36:19 +0900

    🔧 파이어베이스 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "@components/header/Header";
 
+import firebaseConfig from "@constants/firebaseConfig";
+
 import Index from "@pages/index/Index";
 
 import "@utils/loadIpcRenderer";
 
 import "./index.css";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>

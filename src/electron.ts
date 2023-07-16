3eab30da16221802edcdca7deada32dceb500268
Minibox <minibox724@gmail.com>
2023-07-16 18:17:41 +0900

    ✨ 디스코드 RPC

--- src/electron.ts
+++ src/electron.ts
@@ -2,6 +2,8 @@ import { BrowserWindow, app, ipcMain, nativeImage } from "electron";
 import * as isDev from "electron-is-dev";
 import { join } from "path";
 
+import { runDiscordRPC } from "./electron/discord";
+
 (async () => {
   // Initialize the Electron application
   app.whenReady().then(() => {
@@ -33,6 +35,8 @@ import { join } from "path";
     } else {
       win.loadFile(join(__dirname, "../dist/index.html"));
     }
+
+    runDiscordRPC();
   });
 })();
 

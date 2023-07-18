626047025ad61d819df893e8b3f36bf14aeaff21
unsignd <unsigndid@gmail.com>
2023-07-18 21:39:11 +0900

    🧑‍💻 Result 함수 최적화

--- src/components/search/Result.tsx
+++ src/components/search/Result.tsx
@@ -1,5 +1,4 @@
 import { SongsSearchResponse } from "@templates/search.ts";
-import styled from "styled-components";
 
 import NotFound from "./NotFound";
 import All from "./tabs/All";
@@ -12,20 +11,16 @@ interface ResultProps {
 }
 
 const Result = ({ tab, query, res }: ResultProps) => {
-  return (
-    <Container>
-      {tab === "all" &&
-      Object.values(res).filter((value) => value.length !== 0).length !== 0 ? (
-        <All query={query} res={res} />
-      ) : tab !== "all" && res[tab].length !== 0 ? (
-        <List tab={tab} res={res} />
-      ) : (
-        <NotFound />
-      )}
-    </Container>
-  );
+  if (
+    tab === "all" &&
+    Object.values(res)
+      .map((i) => i.length)
+      .some((i) => i !== 0)
+  )
+    return <All query={query} res={res} />;
+  else if (tab !== "all" && res[tab].length !== 0)
+    return <List tab={tab} res={res} />;
+  else return <NotFound />;
 };
-
-const Container = styled.div``;
 
 export default Result;

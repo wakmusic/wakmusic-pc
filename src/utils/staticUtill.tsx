2f0792e2acb83732b71e9dd61802e60ad2d5337b
김벽걸 <biob1121@naver.com>
2023-07-22 14:18:55 +0900

    ✨ : static Util 개발

---
+++ src/utils/staticUtill.tsx
@@ -0,0 +1,55 @@
+const staticURL = "https://static.wakmusic.xyz/static/";
+
+type profileImgType =
+  | "bat"
+  | "dulgi"
+  | "ddong"
+  | "gorani"
+  | "jupock"
+  | "panchi"
+  | "segyun"
+  | "ifari";
+
+type artistImgType = "card" | "big" | "group" | "full" | "round" | "square";
+
+type documentType = "privacy" | "terms";
+
+export const getArtistImg = (type: artistImgType, id: string) => {
+  let URL = `${staticURL}/artist/${type}/${id}.png`;
+  if (type == "big") {
+    URL = `${staticURL}/artist/${type}/${id}.jpg`;
+  }
+  return URL;
+};
+
+export const getLyrics = (id: string) => {
+  return `${staticURL}/lyrics/${id}.vtt`;
+};
+
+export const getProfileImg = (name: profileImgType) => {
+  return `${staticURL}/profile/${name}.png`;
+};
+
+export const getYoutubeThumbnail = (id: string) => {
+  return `https://i.ytimg.com/vi/${id}/hqdefault.jpg`;
+};
+
+export const getAudio = (id: string) => {
+  return `${staticURL}/audio/${id}.mp3`;
+};
+
+export const getPlaylistIconSquare = (id: string) => {
+  return `${staticURL}/playlist/icon/square/${id}.png`;
+};
+
+export const getPlaylistIconRound = (id: string) => {
+  return `${staticURL}/playlist/icon/round/${id}.png`;
+};
+
+export const getPlaylistIcon = (id: number) => {
+  return `${staticURL}/playlist/${id}.png`;
+};
+
+export const getDocument = (type: documentType) => {
+  return `${staticURL}/document/${type}.pdf`;
+};

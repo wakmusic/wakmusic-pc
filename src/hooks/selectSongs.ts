a28115167061d3040eb21c09cbd16f6163982870
sandsunset <sejoon0606@gmail.com>
2023-08-15 15:59:20 +0900

    :fire: 불필요한 코드 제거

--- src/hooks/selectSongs.ts
+++ src/hooks/selectSongs.ts
@@ -22,14 +22,11 @@ export const useSelectSongs = () => {
         _selected.length === _song.length &&
         _selected.every((value, index) => value === _song[index])
       ) {
-        console.log("a");
         setSelected([]);
       } else {
-        console.log("b");
         setSelected(song);
       }
 
-      console.log(song, selected);
       return;
     }
 

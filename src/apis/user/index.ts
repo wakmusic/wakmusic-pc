47620105cac274a61685a45b08d2918e88bddad2
Minibox <minibox724@gmail.com>
2023-08-20 11:58:34 +0900

    🐛 validateStatus를 무조건 true 하지 않게 수정

--- src/apis/user/index.ts
+++ src/apis/user/index.ts
@@ -2,8 +2,12 @@ import instance from "@apis/axios";
 
 import { User, UserProfile } from "@templates/user";
 
+const validateStatus = () => true;
+
 export const fetchUser = async (): Promise<User | null> => {
-  const { data, status } = await instance.get(`/v2/user/profile`);
+  const { data, status } = await instance.get(`/v2/user/profile`, {
+    validateStatus,
+  });
 
   if (status === 200) return data;
 
@@ -19,23 +23,37 @@ export const fetchProfileImages = async (): Promise<UserProfile[]> => {
 export const setProfileImage = async (
   profile: UserProfile
 ): Promise<boolean> => {
-  const { status } = await instance.patch(`/v2/user/profile`, {
-    type: profile.type,
-  });
+  const { status } = await instance.patch(
+    `/v2/user/profile`,
+    {
+      type: profile.type,
+    },
+    {
+      validateStatus,
+    }
+  );
 
   return status === 201;
 };
 
 export const setUsername = async (name: string): Promise<boolean> => {
-  const { status } = await instance.patch(`/v2/user/name`, {
-    name,
-  });
+  const { status } = await instance.patch(
+    `/v2/user/name`,
+    {
+      name,
+    },
+    {
+      validateStatus,
+    }
+  );
 
   return status === 201;
 };
 
 export const removeUser = async (): Promise<boolean> => {
-  const { status } = await instance.delete(`/v2/user/remove`);
+  const { status } = await instance.delete(`/v2/user/remove`, {
+    validateStatus,
+  });
 
   return status === 200;
 };

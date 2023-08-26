5b36245b25d4752f31170b5b2eafd8c6373e0dc5
Minibox <minibox724@gmail.com>
2023-08-27 01:45:49 +0900

    ✨ 유튜브 플레이어 리라이트

---
+++ src/apis/play/index.ts
@@ -0,0 +1,30 @@
+import instance from "@apis/axios";
+
+import { Song } from "@templates/song";
+
+import processSong from "@utils/processSong";
+
+type PrevData = {
+  songId: string;
+  stopedAt: number;
+  songLength: number;
+};
+
+export const requestPlaySong = async (
+  currentSongId: string,
+  prev: PrevData | undefined
+): Promise<Song> => {
+  const { data } = await instance.post(`/v2/play`, {
+    prev,
+    curr: {
+      songId: currentSongId,
+    },
+
+    // TODO: 일단 pc라고 박아두긴 했지만 나중에 어떻게 될지 모름
+    os: "pc",
+  });
+
+  console.log(data);
+
+  return processSong("total", data);
+};
--- src/constants/soundBoosts.ts
+++ src/constants/soundBoosts.ts
@@ -1,9 +1,9 @@
 const soundBoosts: {
   [key: string]: number;
 } = {
-  rFxJjpSeXHI: 5, // 주이언시스트
-  thFYIhd0A30: 7, // 바다의 보배
-  qSucS9YdhVE: 6, // 지구멸망
+  rFxJjpSeXHI: 2, // 주이언시스트
+  thFYIhd0A30: 2, // 바다의 보배
+  qSucS9YdhVE: 2, // 지구멸망
 };
 
 export default soundBoosts;

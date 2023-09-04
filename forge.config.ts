b1530011463e16eb4ca24acc5a487ce27021581f
Minibox <minibox724@gmail.com>
2023-09-04 19:55:51 +0900

    💚 맥 빌드 19트

--- forge.config.ts
+++ forge.config.ts
@@ -1,86 +1,48 @@
-import { MakerAppX } from "@electron-forge/maker-appx";
 import { MakerDMG } from "@electron-forge/maker-dmg";
+import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import type { ForgeConfig } from "@electron-forge/shared-types";
 
 const config: ForgeConfig = {
   packagerConfig: {
-    name: "wakmu",
-    icon: "public/favicon.ico",
+    name: "왁타버스 뮤직",
+    icon: "./build/appicon",
     asar: true,
-
-    ignore: [
-      /.github/,
-      /.vscode/,
-      /.yarn/,
-      /.dist/,
-      /public/,
-      /src/,
-      /.env/,
-      /.editorconfig/,
-      /.eslintrc/,
-      /.gitignore/,
-      /.gitmodules/,
-      /.prettierrc/,
-      /.forge.config.ts/,
-      /README.md/,
-      /tsconfig.json/,
-      /tsconfig.node.json/,
-      /vite.main.config.ts/,
-      /vite.renderer.config.ts/,
-      /node_modules\/.vite/,
-    ],
-
-    protocols: [
-      {
-        name: "왁타버스 뮤직",
-        schemes: ["wakmusic"],
+    appBundleId: "com.wakmusic-pc",
+    ignore: [/.yarn/],
+    osxSign: {
+      type: "distribution",
+      identityValidation: true,
+      identity: process.env.APPLE_IDENTITY,
+      provisioningProfile: process.env.PP_PATH,
+      optionsForFile: (_) => {
+        return {
+          hardenedRuntime: true,
+          entitlements: "./build/macos/entitlements.plist",
+        };
       },
-    ],
-
-    // afterCopy: [
-    //   async (buildPath, _electronVersion, _platform, _arch, callback) => {
-    //     const localeDir = join(buildPath, "../../locales");
-
-    //     const files = await readdir(localeDir);
-
-    //     for (const file of files) {
-    //       if (file !== "en-US.pak") {
-    //         await unlink(join(localeDir, file));
-    //       }
-    //     }
-
-    //     callback();
-    //   },
-    // ],
-
-    // osxSign: {
-    //   preAutoEntitlements: false,
-    //   optionsForFile: (_) => {
-    //     return {
-    //       entitlements: "./build/macos/entitlements.plist",
-    //     };
-    //   },
-    // },
-    // osxNotarize: {
-    //   tool: "notarytool",
-    //   appleId: process.env.VITE_APPLE_ID ?? "",
-    //   appleIdPassword: process.env.VITE_APPLE_PASSWORD ?? "",
-    //   teamId: process.env.VITE_APPLE_TEAM_ID ?? "",
-    // },
+      strictVerify: true,
+      preAutoEntitlements: false,
+      preEmbedProvisioningProfile: false,
+    },
+    osxNotarize: {
+      tool: "notarytool",
+      appleId: process.env.APPLE_ID ?? "",
+      appleIdPassword: process.env.APPLE_PASSWORD ?? "",
+      teamId: process.env.APPLE_TEAM_ID ?? "",
+    },
   },
+  rebuildConfig: {},
   makers: [
-    new MakerAppX({
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-
-      identityName: "55390B70E4627.1221F2615847",
-      publisher: "CN=CFF2F2B0-3997-40A5-9513-E927980AC814",
-      publisherDisplayName: "왁타버스 뮤직",
-
-      assets: "./build/appx",
+    new MakerSquirrel({}),
+    new MakerDMG({
+      additionalDMGOptions: {
+        "code-sign": {
+          "signing-identity": process.env.APPLE_IDENTITY ?? "",
+        },
+      },
+      icon: "./build/appicon.icns",
     }),
-    new MakerDMG({}),
   ],
   plugins: [
     new VitePlugin({

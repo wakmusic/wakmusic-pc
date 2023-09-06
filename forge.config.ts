fcd69762b8cec39eb3cb94d0ca7c95addbbe7a2d
Minibox <minibox724@gmail.com>
2023-09-06 22:52:29 +0900

    👷 x64로 변경

--- forge.config.ts
+++ forge.config.ts
@@ -111,7 +111,7 @@ const config: ForgeConfig = {
           return [
             {
               type: "file",
-              path: `${process.cwd()}/out/wakmusic-pc-darwin-arm64/wakmusic-pc.app`,
+              path: `${process.cwd()}/out/wakmusic-pc-darwin-x64/wakmusic-pc.app`,
               x: 292,
               y: 290,
             },

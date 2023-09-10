14fd8d540d49bdf61d047e2236bd88e07b54f350
sandsunset <sejoon0606@gmail.com>
2023-09-10 15:34:44 +0900

    :recycle: selectInculdes 리펙토링

--- src/hooks/selectSongs.ts
+++ src/hooks/selectSongs.ts
@@ -9,14 +9,42 @@ export const useSelectSongs = (songs: Song[]) => {
 
   useEffect(() => {
     // select만 바뀐경우, songs의 곡이 삭제된 경우
-    selected.forEach((item) => {
-      item.index = songs.findIndex((song) => song.songId === item.songId);
-    });
+    // selected.forEach((item) => {
+    //   item.index = songs.findIndex((song) => song.songId === item.songId);
+    // });
+    console.log(selected);
   }, [songs, selected]);
+
+  const findIndex = (from: Song[] | OrderedSongType[], songId: string) => {
+    let index = -1;
+
+    for (let target = 0; target < Math.ceil(from.length / 2); target++) {
+      if (from[target].songId === songId) {
+        index = target;
+        break;
+      }
+
+      const targetOpposite = from.length - target - 1;
+      if (targetOpposite != target && from[targetOpposite].songId === songId) {
+        index = targetOpposite;
+        break;
+      }
+    }
+
+    return index;
+  };
 
   const selectCallback = (song: Song | Song[], index?: number) => {
     if (Array.isArray(song)) {
       const newSelected: OrderedSongType[] = [];
+
+      if (
+        song.length === selected.length &&
+        song.every((value) => findIndex(songs, value.songId) !== -1)
+      ) {
+        setSelected([]);
+        return;
+      }
 
       song.forEach((item) => {
         if (selectedIncludes(item)) {
@@ -25,7 +53,7 @@ export const useSelectSongs = (songs: Song[]) => {
 
         newSelected.push({
           ...item,
-          index: songs.findIndex((value) => value.songId === item.songId),
+          index: findIndex(song, item.songId),
         });
       });
 
@@ -36,9 +64,7 @@ export const useSelectSongs = (songs: Song[]) => {
     if (isUndefined(index)) return;
 
     const newSelected = [...selected];
-    const songIndex = selected.findIndex(
-      (value) => value.songId === song.songId
-    );
+    const songIndex = findIndex(selected, song.songId);
 
     if (songIndex === -1) {
       newSelected.push({
@@ -53,8 +79,7 @@ export const useSelectSongs = (songs: Song[]) => {
   };
 
   const selectedIncludes = useCallback(
-    (song: Song) =>
-      selected.findIndex((value) => value.songId === song.songId) !== -1,
+    (song: Song) => findIndex(selected, song.songId) != -1,
     [selected]
   );
 

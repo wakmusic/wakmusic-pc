d7bd7793b4c01d95b9abdaf1618cd5b0f74a2143
Minibox <minibox724@gmail.com>
2023-09-18 08:47:01 +0900

    🐛 무한으로 뜨는 안내 모달

--- src/components/index/Splash.tsx
+++ src/components/index/Splash.tsx
@@ -23,6 +23,8 @@ const Splash = ({}: SplashProps) => {
     import.meta.env.VITE_NO_SPLASH === "true"
   );
 
+  const [isShowAlert, setIsShowAlert] = useState(false);
+
   const [, setIsNoticeModalOpen] = useNoticeModalState();
 
   const alert = useAlertModal();
@@ -35,13 +37,15 @@ const Splash = ({}: SplashProps) => {
   }, [controls, setIsNoticeModalOpen]);
 
   useEffect(() => {
-    if (disable && isUndefined(ipcRenderer)) {
+    if (disable && !isShowAlert && !isUndefined(ipcRenderer)) {
       alert(
         "웹 버전 이용 안내",
         "현재 PC 앱을 일반 브라우저로 접속하여 이용하는 버전은 정식으로 제공하는 기능이 아닙니다. 따라서 일부 기능이 PC 앱과 다르게 작동할 수 있으니 양해 부탁드립니다."
       );
+
+      setIsShowAlert(true);
     }
-  }, [disable, alert]);
+  }, [disable, alert, isShowAlert]);
 
   if (disable) {
     return null;

537aa52d9ca6d1444330f5ca55b46b4f250cc56a
Minibox <minibox724@gmail.com>
2023-09-18 20:26:48 +0900

    🐛 공지가 한개 이상일때만 인디케이터 표시

--- src/components/modals/NoticeModal.tsx
+++ src/components/modals/NoticeModal.tsx
@@ -90,11 +90,13 @@ const NoticeModal = ({}: NoticeModalProps) => {
           ))}
         </Swiper>
 
-        <ProgressContainer>
-          <T6Medium color={colors.blueGray25}>
-            {index + 1} / {notices.length}
-          </T6Medium>
-        </ProgressContainer>
+        {notices.length > 1 && (
+          <ProgressContainer>
+            <T6Medium color={colors.blueGray25}>
+              {index + 1} / {notices.length}
+            </T6Medium>
+          </ProgressContainer>
+        )}
 
         <Buttons>
           <Button $color={colors.blueGray400} onClick={() => close(true)}>
